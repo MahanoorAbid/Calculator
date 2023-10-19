9d3248fb64f17ff3a42eccfcfbef9844482af5b9
Mahnoor Abid <noorweb909@gmail.com>
2023-10-19 23:00:12 +0500

    Initial commit

---
+++ node_modules/calculator-assignmentt/index.ts
@@ -0,0 +1,63 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answers : {
+    numberOne: number,
+    numberTwo: number,
+    operator: string
+
+}
+= await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "kindly enter your first no:"
+    }, 
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "kindly enter your second no:"
+    },   
+    {
+        type: "list",
+        name: "operator",
+        choices: ["*", "-", "+", "/"],
+        message: "kindly select operator:"
+    },       
+]);
+const {numberOne, numberTwo, operator} = answers;
+if (numberOne && numberTwo && operator) {
+    let result: number = 0;
+    if (operator === "+") {
+        result = numberOne + numberTwo
+        console.log("Addition:", result)
+
+        
+    }
+    
+    else if (operator === "-") {
+        result = numberOne - numberTwo
+        console.log("Subtraction:", result)
+
+        
+
+        
+    }
+    if (operator === "/") {
+        result = numberOne / numberTwo
+        console.log("Dvision:", result)
+
+        
+    }
+    if (operator === "*") {
+        result = numberOne * numberTwo
+        console.log("Multiplication:", result)
+
+        
+    }
+} else{
+    console.log(
+        "enter your valid number"
+    )
+} 
